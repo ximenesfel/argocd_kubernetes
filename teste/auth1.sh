0fadfff709c536e25aeb7d6dccf26d6779f39e9a
jjader <jamisson.junior@a3data.com.br>
2023-03-20 08:51:01 -0300

    adicionando alguns testes

--- teste/auth1.sh
+++ teste/auth1.sh
@@ -5,5 +5,5 @@ AWS_ACCOUNT_ID=127012818163
 AWS_REGION=us-east-1
 AWS_CLI_VERSION=2
 
-aws ecr --region $AWS_REGION get-authorization-token --output text --query 'authorizationData[].authorizationToken' | base64 -d
+aws ecr --region us-east-1 get-authorization-token --output text --query 'authorizationData[].authorizationToken' | base64 -d
 
